b31e1660185a18ba59f278e2e6bfaee58f8172b6
ForexPro <roma.minihanowa@mail.ru>
2022-11-16 02:03:36 +0400

    del dist

--- dist/js/about.c0849bf7.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkpolomka"]=self["webpackChunkpolomka"]||[]).push([[443],{272:function(n,t,u){u.r(t),u.d(t,{default:function(){return i}});var s=function(){var n=this;n._self._c;return n._m(0)},a=[function(){var n=this,t=n._self._c;return t("div",{staticClass:"about"},[t("h1",[n._v("This is an about page")])])}],l=u(1),e={},r=(0,l.Z)(e,s,a,!1,null,null,null),i=r.exports}}]);
-//# sourceMappingURL=about.c0849bf7.js.map
