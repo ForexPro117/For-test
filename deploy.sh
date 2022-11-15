1ab24a81bd4c3808df899bdb56a541310e2e2d0b
ForexPro <roma.minihanowa@mail.ru>
2022-11-15 23:08:04 +0400

    anoter fix

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,25 @@
 #!/usr/bin/env sh
 
-# остановить публикацию при ошибках
+# abort on errors
 set -e
 
-# сборка
+# build
 npm run build
 
-# переход в каталог сборки
+# navigate into the build output directory
 cd dist
 
-# если вы публикуете на пользовательский домен
+# if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:forexpro117/for-test.git main:gh-pages
+
+cd -
